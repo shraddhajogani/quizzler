4d4141432a452f802f8cfeb8383842afac9f075f
Shraddha <jogani.shraddha@gmail.com>
2019-09-03 13:11:35 +0200

    *In quiz_brain + import question.dart +Create a class called QuizBrain +Copy all question from README file +Create a NextQuestion function +Create a string and bool to get question
    +Add New dart file called quiz_brain
    
    * In Main_dart
    +Add List of question to Quiz_brain
    +Replace QuestionBank with quiz.getQuestionText()
    
    *In Question dart
    +Create a new object called newQuestion

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
 
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,27 +28,16 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
- // List<Icon> scoreKeeper = [];
+  // List<Icon> scoreKeeper = [];
   //List<String> questions = [
-   // 'You can lead a cow down stairs but not up stairs.',
-    //'Approximately one quarter of human bones are in the feet.',
-    //'A slug\'s blood is green.'
+  // 'You can lead a cow down stairs but not up stairs.',
+  //'Approximately one quarter of human bones are in the feet.',
+  //'A slug\'s blood is green.'
   //];
 
   //List<bool> answers=[false, true, false,];
 
   //Question q1 = Question(q: 'You can lead a cow down stairs but not up stairs.',a: false);
-
-  List<Question> questionBank= [
-    Question(q:'You can lead a cow down stairs but not up stairs.',a: false),
-  Question(q: 'Approximately one quarter of human bones are in the feet.',a: true),
-    Question(q:'A slug\'s blood is green.',a:true),
-  ];
-
-
-
-
-  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-               questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -86,19 +76,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool corectAnswer = questionBank[questionNumber].questionAnswer;
-                if (corectAnswer== true){
+                bool corectAnswer = quizBrain.getCorrectAnswer();
+                if (corectAnswer == true) {
                   print('User got right');
-                }
-                else{
-                  print ('User got wrong');
-
+                } else {
+                  print('User got wrong');
                 }
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
+
               },
             ),
           ),
@@ -116,26 +104,27 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool corectAnswer = questionBank[questionNumber].questionAnswer;
-                if(corectAnswer== true){
-                  print('User got right');
-                }
-                else{
-                  print ('User got wrong');
+                // The user picked true
 
+                //(If you press false all the time still it will show that user get right answer) quizBrain.questionBank[questionNumber].questionAnswer=true;
+
+                bool corectAnswer = quizBrain.getCorrectAnswer();
+
+                if (corectAnswer == true) {
+                  print('User got right');
+                } else {
+                  print('User got wrong');
                 }
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
-                print(questionNumber);
+
                 //The user picked false.
               },
             ),
           ),
         ),
-        Row(
-
-        ),
+        Row(),
       ],
     );
   }
